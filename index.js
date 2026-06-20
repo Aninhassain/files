f4d10e54231746b61768a5a6ecfb2b2dfa5a7cc6
Anin Hassain <aninhassain@github.com>
2026-06-20 23:59:22 +0530

    feat: add file reading functionality and display list of files in index view

--- index.js
+++ index.js
@@ -7,8 +7,26 @@ app.use(express.json()); // middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 
 app.get('/', function (req, res) {
-  res.render('index');
+  fs.readdir('./public/allfiles/', function(err, files) {
+    res.render('index', { allfiles: files || [] });
+  });
 });
+app.get('/read/:filename',function(req,res){
+     const filename= req.params.filename +'.txt';
+     fs.readFile(`./public/allfiles/${filename}`,'utf-8',function(err,data){
+        if(err){
+            res.render('index');
+        }
+        if(data.length>0){
+             res.render('read',{data:data});
+        }
+        else{
+            console.log("no data");
+            res.redirect('/');
+        }
+  
+     })
+})
 app.post('/create', function (req, res){
     const filename=req.body.filename.split(' ').join('');
     const content=req.body.content;
@@ -17,6 +35,7 @@ app.post('/create', function (req, res){
     });
    res.redirect('/');
 })
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
