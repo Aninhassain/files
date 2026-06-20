76bfe694d36db545c26b9d63cc772840b39f23d3
Anin Hassain <aninhassain@github.com>
2026-06-20 21:35:55 +0530

    feat: implement file creation feature with EJS rendering and styling

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+app.set('view engine', 'ejs'); // Set EJS as the view engine
+
+app.use(express.json()); // middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
+app.post('/create', function (req, res){
+    const filename=req.body.filename.split(' ').join('');
+    const content=req.body.content;
+    fs.writeFile(`./public/allfiles/${filename}.txt`, content, function(err){
+        if(err) console.log(err);
+    });
+   res.redirect('/');
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
